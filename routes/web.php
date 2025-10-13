97c2a0355a9055c43f0e7bd392204095ad2f4a6c
Arthur França <arthur.fortes@yardex.com.br>
2025-10-13 16:46:55 -0300

    adicionando as primeiras rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'Hello, World!';
 });
+
+Route::get('/about', function () {
+    return 'About Us';
+});
+
+Route::get('/contact', function () {
+    return 'Contact Us';
+});
